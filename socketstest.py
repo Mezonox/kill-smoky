c6c09608c9df48f2201ea4445b4f15b617031e05
dion.moses <phoncey@gmail.com>
2014-02-02 12:09:48 -0800

    Added SkeletonPython.py and socketstest.py

---
+++ socketstest.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import getopt
+import sys
+
+def main():
+    parseCommandLine()
+    
+def useage():
+    print "Do it better!"    
+    
+def parseCommandLine():
+    try: 
+        opts,args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "output="])
+    except getopt.GetoptError as err:
+        #pint help info and exit
+        print(err)
+        useage()
+        sys.exti(2)
+    output = None
+    verbose = False
+    for o,a in opts:
+        if o == "-v":
+            verbose = True
+        elif o in ("-h", "--help"):
+            useage()
+            sys.exit()
+        elif o in ("-o", "--output"):
+            output = a
+        else:
+            assert False, "unhandled option"
+                    
+
+
+if __name__ == '__main__':
+    main()
