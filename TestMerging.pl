749790c900299a2042c48cb44bce2cc3b29ae378
dion.moses <phoncey@gmail.com>
2014-04-06 11:53:41 -0500

    Small Change to TestMerging.pl

--- TestMerging.pl
+++ TestMerging.pl
@@ -48,7 +48,7 @@ sub ChangeFiles{
 		open($filehandle, ">", $file) or die "Can't open $file for output $!";
 		foreach my $line(@origFileContents){
 			#chomp $line;
-			#every 20 slocs
+			#every 30 slocs
 			if($counter % 30 == 0){
 				print $filehandle $alterContents[$counter];
 			}
